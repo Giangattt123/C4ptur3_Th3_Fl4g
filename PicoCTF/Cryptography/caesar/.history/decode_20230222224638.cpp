dbf495bd6b8f9b4c09abb60c864fc29f54b62f63
Giangattt123 <dodaihocneu123@gmail.com>
2023-02-23 11:23:03 +0700

    PicoCTF_Crypto

---
+++ PicoCTF/Cryptography/caesar/.history/decode_20230222224638.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define long long ll;
+void decode(string s){
+    string temp = s;
+    for(int i = 1 ; i <= 25 ; i++){
+        string tmp = "";
+        int key_number = i;
+        for(int j = 0 ; j < s.length() ; j++){
+            s[j] = char((s[j])+ (key_number % 26));
+            if(s[j] > 122)
+                s[j] -= 26;
+            tmp += s[j];
+        }
+        cout << tmp << endl;
+        s = temp;
+    }
+}
+int main(void){
+    string plain_text = "ynkooejcpdanqxeykjrbdofgkq";
+    decode(plain_text);
+}
