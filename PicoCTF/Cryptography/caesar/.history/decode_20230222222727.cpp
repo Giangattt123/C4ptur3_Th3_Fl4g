83ef6815416909cbc6d068805594e60e728b1d95
Giangattt123 <dodaihocneu123@gmail.com>
2023-02-23 11:23:03 +0700

    PicoCTF_Crypto

---
+++ PicoCTF/Cryptography/caesar/.history/decode_20230222222727.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define long long ll;
+void decode(string s){
+    for(int i = 1 ; i <= 25 ; i++){
+        string tmp = "";
+        int key_number = i;
+        for(int j = 0 ; j < s.length() ; j++){
+            s[j] = char(s[j] + (key_number % 26));
+            tmp += s[j];
+        }
+        cout << tmp << endl;
+    }
+}
+int main(void){
+    string plain_text = "ynkooejcpdanqxeykjrbdofgkq";
+    decode(plain_text);
+}
---
+++ PicoCTF/Cryptography/caesar/.history/decode_20230222223959.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define long long ll;
+void decode(string s){
+    for(int i = 1 ; i <= 25 ; i++){
+        string tmp = "";
+        int key_number = i;
+        for(int j = 0 ; j < s.length() ; j++){
+            s[j] = char((s[j])+ (key_number % 26));
+            if(s[j] > 122)
+                s[j] -= 26;
+            tmp += s[j];
+        }
+        cout << tmp << endl;
+    }
+}
+int main(void){
+    string plain_text = "ynkooejcpdanqxeykjrbdofgkq";
+    decode(plain_text);
+}
